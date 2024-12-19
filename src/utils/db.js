02740cbbf595e7ac1eab00980379e054ca02a411
jasonetorres <jasontorres585@icloud.com>
2024-12-19 08:34:48 -0500

    shit

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,6 +1,7 @@
-import { createClient } from '@astrajs/collections';
 import dotenv from 'dotenv';
 dotenv.config();
+
+import { createClient } from '@astrajs/collections';
 
 console.log('Database ID:', process.env.ASTRA_DB_ID);
 console.log('Database Region:', process.env.ASTRA_DB_REGION);
@@ -12,9 +13,12 @@ const client = createClient({
     baseUrl: `https://${process.env.ASTRA_DB_ID}-${process.env.ASTRA_DB_REGION}.apps.astra.datastax.com`,
 });
 
+// Correct way to get a collection
 export const getCollection = async (collectionName) => {
-    const collection = client
-        .keyspace('recipes')
-        .collection(collectionName);
+    const collection = client.collection('default_keyspace', collectionName); // Use the collectionName parameter
     return collection;
+};
+
+export const getRecipesCollection = async () => {
+    return await getCollection('recipes'); // Access the 'recipes' collection directly
 };
