ca3f8f32b59a16b0d7d7eabec314def4fe57b701
jasonetorres <jasontorres585@icloud.com>
2024-12-23 07:23:14 -0500

    added state value to hide recipe cards on start

--- src/components/RecipeIsland.tsx
+++ src/components/RecipeIsland.tsx
@@ -4,7 +4,8 @@ import Searchbar from "./SearchBar";
 import RecipeCard from "./RecipeCard";
 
 export default function RecipeIsland({ recipes }: { recipes: Recipe[] }) {
-  const [filteredRecipes, setFilteredRecipes] = useState<Recipe[]>(recipes);
+  const [filteredRecipes, setFilteredRecipes] = useState<Recipe[]>([]);
+  const [searchPerformed, setSearchPerformed] = useState(false);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -12,7 +13,7 @@ export default function RecipeIsland({ recipes }: { recipes: Recipe[] }) {
     const formData = new FormData(form);
     const searchTerm = formData.get("search") as string;
 
-    const searchResults = filteredRecipes
+    const searchResults = recipes
       .filter(
         (recipe) =>
           recipe.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
@@ -27,39 +28,47 @@ export default function RecipeIsland({ recipes }: { recipes: Recipe[] }) {
 
     console.log(searchResults);
     setFilteredRecipes(searchResults);
+    setSearchPerformed(true);
   };
 
   const handleReset = () => {
-    setFilteredRecipes(recipes);
+    setFilteredRecipes([]);
+    setSearchPerformed(false);
   };
 
   return (
     <div className="flex flex-col items-center justify-center">
-      {filteredRecipes.length > 0 ? (
-        <div className="space-y-8" data-recipes-container>
-          <Searchbar onSubmit={handleSubmit} handleReset={handleReset} />
-          {filteredRecipes.map((recipe, index) => (
-            <RecipeCard
-              key={`recipe-${index}-${recipe.title.toLocaleLowerCase()}`}
-              cookTime={recipe.cookTime}
-              description={recipe.description}
-              difficulty={recipe.difficulty}
-              ingredients={recipe.ingredients}
-              instructions={recipe.instructions}
-              prepTime={recipe.prepTime}
-              title={recipe.title}
-              totalTime={recipe.totalTime}
-            />
-          ))}
-        </div>
-      ) : (
-        <div>
-          <p>No recipes found.</p>
-          <button onClick={handleReset} className="mt-5 mb-5 p-3 text-white text-center rounded" style={{ backgroundColor: '#E11D48' }} >
-            Back to Search
-          </button>
-        </div>
-      )}
+      <Searchbar onSubmit={handleSubmit} handleReset={handleReset} />
+      {searchPerformed ? (
+        filteredRecipes.length > 0 ? (
+          <div className="space-y-8" data-recipes-container>
+            {filteredRecipes.map((recipe, index) => (
+              <RecipeCard
+                key={`recipe-${index}-${recipe.title.toLocaleLowerCase()}`}
+                cookTime={recipe.cookTime}
+                description={recipe.description}
+                difficulty={recipe.difficulty}
+                ingredients={recipe.ingredients}
+                instructions={recipe.instructions}
+                prepTime={recipe.prepTime}
+                title={recipe.title}
+                totalTime={recipe.totalTime}
+              />
+            ))}
+          </div>
+        ) : (
+          <div>
+            <p>No recipes found.</p>
+            <button
+              onClick={handleReset}
+              className="mt-5 mb-5 p-3 text-white text-center rounded"
+              style={{ backgroundColor: '#E11D48' }}
+            >
+              Back to Search
+            </button>
+          </div>
+        )
+      ) : null}
     </div>
   );
 }
